1cdf0276bc2041521f0c338e83c9b1332e9345e5
Albert Aranda <albert.aranda.peraza@gmail.com>
2026-05-09 15:01:03 -0700

    added mock express data for posts and users

---
+++ server/index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+import { users, posts } from "./mockData.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Social media API running");
+});
+
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  res.json(user);
+});
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.get("/api/posts/:id", (req, res) => {
+  const postId = Number(req.params.id);
+  const post = posts.find((p) => p.id === postId);
+
+  if (!post) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  res.json(post);
+});
+
+app.get("/api/users/:id/posts", (req, res) => {
+  const userId = Number(req.params.id);
+  const userPosts = posts.filter((p) => p.userId === userId);
+
+  res.json(userPosts);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on http://localhost:3000");
+});
---
+++ server/mockData.js
@@ -0,0 +1,74 @@
+export const users = [
+  {
+    id: 1,
+    name: "Albert",
+    username: "@albert",
+    postCount: 7,
+    followers: 123,
+    following: 72,
+  },
+
+    {
+      id: 2,
+      name: "Joe",
+      username: "@Joe12",
+      postCount: 2,
+      followers: 21,
+      following: 102,
+  },
+
+    {
+      id: 3,
+      name: "Sally",
+      username: "@SallyWag",
+      postCount: 3,
+      followers: 4567,
+      following: 41,
+  },
+];
+
+export const posts = [
+  {
+    id: 1,
+    userId: 1,
+    game: "Minecraft",
+    content: "#Speedrun",
+    tag: "#Nostalgia",
+    likes: 157,
+    commentCount: 22,
+    shareCount: 34,
+  },
+
+    {
+    id: 2,
+    userId: 1,
+    game: "Clash of Clans",
+    content: "#Gameplay",
+    tag: "#Funny",
+    likes: 27,
+    commentCount: 6,
+    shareCount: 21,
+  },
+
+  {
+    id: 3,
+    userId: 2,
+    game: "Call of Duty",
+    content: "#Gameplay",
+    tag: "#Commentary",
+    likes: 47,
+    commentCount: 61,
+    shareCount: 54,
+  },
+
+  {
+    id: 4,
+    userId: 3,
+    game: "Fortnite",
+    content: "#Trolling",
+    tag: "#Funny",
+    likes: 1002,
+    commentCount: 123,
+    shareCount: 200,
+  },
+];
