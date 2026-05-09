37b12ec762323c4853ac9164ebb73394f7e11965
Albert Aranda <albert.aranda.peraza@gmail.com>
2026-05-09 15:28:53 -0700

    profile test view also printing

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,19 @@
-import { useParams } from "react-router";
+import { useLoaderData, useParams } from "react-router";
 
 export function ProfilePage() {
   const { id } = useParams();
+  const profile = useLoaderData()
 
   return (
     <>
       <h1>Single Profile View Page</h1>
       <p>Profile ID {id}</p>
+
+      <h2>{profile.name}</h2>
+      <p>Username: {profile.username}</p>
+      <p>Posts: {profile.postCount}</p>
+      <p>Followers: {profile.followers}</p>
+      <p>Following: {profile.following}</p>
     </>
   );
 }
