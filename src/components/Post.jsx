0490f4041908d696dc0eba5126f6444ff6953200
Albert Aranda <albert.aranda.peraza@gmail.com>
2026-05-14 22:46:06 -0700

    changed post styling to match Figma style

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,32 @@
 export function Post({ game, content, tag, likes, commentCount, shareCount, image }) {
   return (
     <>
-      <hr />
-      <h2>{game}</h2>
-      <p>Content: {content}</p>
-      <p>Tag: {tag}</p>
-      <p>Likes: {likes}</p>
-      <p>Comments: {commentCount}</p>
-      <p>Shares: {shareCount}</p>
-      <img src={image} alt={game} width="300" />
+    {/* Main card wrapper is post-card */}
+    <article className="post-card">
+      <div className="post-avatar">👤</div>
+
+      {/* Includes main post content */}
+      <div className="post-main">
+        <div className="post-header">
+          <span>PERSON NAME</span>
+          <span>@USERNAME</span>
+        </div>
+
+        <p className="post-text">
+          {game} {content}
+        </p>
+
+        <p className="post-tag">{tag}</p>
+
+        {image && <img className="post-image" src={image} alt={game} />}
+
+        <div className="post-actions">
+          <span>❤️ {likes}</span>
+          <span>💬 {commentCount}</span>
+          <span>📩 {shareCount}</span>
+        </div>
+      </div>
+    </article>
     </>
   );
 }
