c25439b92de5654c7ec06e27993f52173d51f258
Albert Aranda <albert.aranda.peraza@gmail.com>
2026-05-29 18:05:59 -0700

    added logout functionality

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,41 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import toast from "react-hot-toast";
 
 export function Navigation() {
-    const user = JSON.parse(localStorage.getItem("User"));
+  const user = JSON.parse(localStorage.getItem("User"));
+  // Handles Logout
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("User");
+    localStorage.removeItem("token");
+
+    toast.success("Logged out.");
+    navigate("/login");
+  };
   return (
     <nav className="sidebar">
       <div className="logo">🏁 CheckPoint</div>
+
       <Link to="/">🏠 Home</Link>
       <Link to="/messages">💬 Messages</Link>
-      <Link to={user ? `/profile/${user.id}` : "/login"}>👤 Profile</Link>
+
+      <Link to={user ? `/profile/${user.id}` : "/login"}>
+        👤 Profile
+      </Link>
+
       <Link to="/post/1">➕ Post</Link>
-      <Link to="/login">Sign In</Link>
-      <Link to="/signup">Sign Up</Link>
+
+      {/* if user is sign-in display logout button otherwise display sign in*/}
+      {!user ? (
+        <>
+          <Link to="/login">Sign In</Link>
+        </>
+      ) : (
+        <button onClick={handleLogout} className="logout-btn">
+          🚪 Log Out
+        </button>
+      )}
     </nav>
   );
 }
