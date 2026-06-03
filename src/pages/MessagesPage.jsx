bf96055b2893848e91dcadeabb54a2771e01e72a
bitewr <bitewr@wwu.edu>
2026-06-03 02:11:32 +0100

    updated MessagesPage.jsx to handle sending message

--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -2,15 +2,53 @@ import { useLoaderData } from "react-router";
 import { useState } from "react";
 import "./MessagesPage.css"
 
+
 export function MessagesPage() {
   const { messages } = useLoaderData();
+  const [messageList, setMessageList] = useState(messages);
   const [input, setInput] = useState("");
+ 
+  const user = JSON.parse(localStorage.getItem("User"));
 
-  const handleSendMessage = () => {
+
+  const handleSendMessage = async () => {
     if (!input.trim()) return;
 
-    setInput("");
-  }
+    let recipient = "";
+    let messageText = input.trim();
+    if(input.startsWith("@")){
+      //everything a space after the first @ word is the message
+      const firstSpaceIndex = input.indexOf(" ");
+      if(firstSpaceIndex !== -1){
+        recipient = input.slice(0, firstSpaceIndex);
+        messageText = input.slice(firstSpaceIndex + 1).trim();
+      } else {
+        recipient = input.trim();
+        messageText = "";
+      }
+    }
+    if(!messageText) return;
+
+    try {
+      
+      const response = await fetch("/api/messages", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ messageNum: Date.now(), Sender: `@${user.username}`, Recipient: recipient, Message: messageText }),
+      });
+
+      if(!response.ok){
+        console.error("Failed to send message");
+        return;
+      }
+
+      const data = await response.json();
+      setMessageList((prevMessages) => [...prevMessages, data.data]);
+      setInput("");
+    } catch(error) {
+      console.error("Send message error: ", error);
+    }
+  };
 
   return (
     <>
@@ -28,7 +66,7 @@ export function MessagesPage() {
           <div className="people">
             <h1>People</h1>
 
-            {messages.map((message) => (
+            {messageList.map((message) => (
             <div className="message" key={`${message.Sender}-${message.Recipient}-${message.messageNum}`}>
               <p>
                 <strong>{message.Sender}</strong> to <strong>{message.Recipient}</strong>
