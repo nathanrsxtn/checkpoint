b9027d763b5ec1b40be40d13cd3891d74a5ec627
nathanrsxtn <37890449+nathanrsxtn@users.noreply.github.com>
2026-06-08 18:18:37 -0700

    Fix server

--- server/vite.js
+++ server/vite.js
@@ -1,4 +1,4 @@
-import app from "./index.js";
+import app from "./server.js";
 
 export default function express() {
   return {
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import express from "./server/vite.js";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss(), express()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: `${env.VITE_API_BASE_URL}`,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
